4664fda03e4b05a8e3e7adee0270720e640d19db
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-08-09 13:56:40 +0300

    Task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines a function, `number_of_subscribers`"""
+
+
+def top_ten(subreddit):
+    """
+    Return the number of subscribers (not active users, total
+    subscribers) for a given subreddit
+    Args:
+        subreddit (str) - subreddit
+    """
+    import requests
+
+    headers = {"User-Agent": "Python/requests"}
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code == 200:
+        for i in res.json()['data']['children']:
+            print(i['data']['title'])
+    print(None)
