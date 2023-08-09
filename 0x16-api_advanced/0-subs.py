971119a6f6305fbacb374f24d8195242033d974d
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-08-09 11:08:48 +0300

    Import outside function

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Defines a function, `number_of_subscribers`"""
+import requests
 
 
 def number_of_subscribers(subreddit):
@@ -7,7 +8,6 @@ def number_of_subscribers(subreddit):
     Return the number of subscribers (not active users, total
     subscribers) for a given subreddit
     """
-    import requests
 
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     res = requests.get(url)
